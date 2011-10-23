a6b5a1f229d05f0140b1aacbba682095f546e719
Sam Liu <ontouchstart@gmail.com>
2011-10-23 09:14:45 -0400

    git branch CSNotes

--- JSNotes/11-10-11/helloWorldBox.js
+++
@@ -1,9 +0,0 @@
-function helloWorldBox () {
-  var box = el(document.body, 'div', 'hello-box');
-  box.innerHTML = "Hello, World!";
-  box.style.border = "1px solid red";
-  box.style.width = "15em";
-  box.style.padding = "1em";
-  box.style.textAlign = "center";
-  box.style.margin = 'auto';
-}
--- JSNotes/11-10-13/el.js
+++
@@ -1,7 +0,0 @@
-function el(parent, tag, id, className) {
-  var element = document.createElement(tag);
-  if(id) element.id = id;
-  if(className) element.className = className;
-  parent.appendChild(element);
-  return element;
-}
