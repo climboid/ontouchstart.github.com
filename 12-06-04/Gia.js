348627d0ae2eb8d9c54d89b2c7e045ef490f8c91
Sam Liu <ontouchstart@gmail.com>
2012-06-04 08:39:23 -0400

    http://ontouchstart.github.com/12-06-04/Gia.html (svg clones with random letters)

--- 12-06-04/Gia.js
+++ 12-06-04/Gia.js
@@ -1,9 +1,16 @@
 (function () {
   var svg = document.getElementsByTagName('svg')[0];
   svg.style.border = '1px solid';
+  function randomLetter () {
+    var r = Math.random() * 10;
+    if (r > 6) {return 'G'};
+    if (r > 3) {return 'i'};
+    return 'a';
+  }
   for(var i = 0; i < 99; i++) {
     var s = svg.cloneNode(true);
     document.body.appendChild(s);
+    s.getElementsByTagName('text')[0].firstChild.nodeValue = (randomLetter() + randomLetter() + randomLetter());
   }
 
 })();
