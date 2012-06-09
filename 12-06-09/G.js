143a096e621ba95b4b46f4ba720ee56460710de4
Sam Liu <ontouchstart@gmail.com>
2012-06-09 14:33:40 -0400

    http://ontouchstart.github.com/12-06-09

--- 12-06-09/G.js
+++ 12-06-09/G.js
@@ -3,11 +3,15 @@
   var fn = 'G';
   var xml = path + '/' + fn + '.xml';
   var container = document.getElementById(xml);
+  container.style.position = 'absolute';
+  container.style.left = '0';
+  container.style.top = '0';
+
   var svg = container.getElementsByTagName('svg')[0];
   svg.style.display = 'block';
   svg.style.border = '1px solid';
   var glyphList = svg.getElementsByTagName('glyph');
   for (var i = 0; i < glyphList.length; i++) {
-//    console.log(glyphList[i].getAttribute('d'));
+    console.log(glyphList[i].getAttribute('d'));
   }
 })();
--- 12-06-09/path.js
+++ 12-06-09/path.js
@@ -3,6 +3,10 @@
   var fn = 'path';
   var xml = path + '/' + fn + '.xml';
   var container = document.getElementById(xml);
+  container.style.position = 'absolute';
+  container.style.left = '0';
+  container.style.top = '0';
+
   var svg = container.getElementsByTagName('svg')[0];
   svg.style.display = 'block';
   svg.style.border = '1px solid';
@@ -11,13 +15,15 @@
   var Gpath = G.getElementsByTagName('glyph')[0].getAttribute('d');
  
   container.ontouchstart = function () {
-    var path = this.getElementsByTagName('path')[0];    
+    G.style.display = 'none';
+    var path = svg.getElementsByTagName('path')[0];    
     path.setAttribute('d', Gpath);
     console.log('update path to ' + path.getAttribute('d'));
     this.ontouchstart = function() {
       path.parentNode.setAttribute('transform', 'translate(0, 300) scale(1,-1)');
       console.log('flip');
       this.ontouchstart = null;
+      G.style.display = 'block';
     }
   }
   
