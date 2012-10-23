e3925673dbd05f329c5a2cb3b2f18c3c86220cf4
Sam Liu <ontouchstart@gmail.com>
2012-10-22 21:51:06 -0400

    http://ontouchstart.github.com/12-10-22/#pong

--- 12-10-22/pong.js
+++ 12-10-22/pong.js
@@ -13,11 +13,15 @@
 
     var w = root.innerWidth/2;
     var h = w / 1.618;
-    var r = 10;
-    var x = w/2;
-    var y = h/2;
+    var r = w / 40;
+    var x = w / 2;
+    var y = h / 2;
     var dx = 5;
     var dy = 5;
+
+    var leftY = h/2;
+    var rightY = h/2;
+
     var ball = svg.append("circle")
         .attr({
           fill: "#FFF",
@@ -25,7 +29,26 @@
           cx: x,
           cy: y
         });
-    var dt = 10;
+
+    var leftPad = svg.append("rect")
+        .attr({
+          fill: "#FFF",
+          x: 1,
+          y: leftY - h/10,
+          width: w/40,
+         height: h/5
+        });
+
+    var rightPad = svg.append("rect")
+        .attr({
+          fill: "#FFF",
+          x: w - w/40 -1,
+          y: rightY - h/10,
+          width: w/40,
+          height: h/5
+        });
+
+    var dt = 5;
 
     svg.style({
         width: w,
@@ -35,28 +58,71 @@
     });
 
     function bounce() {
-      if(x+dx > w - r) {
-        x = w-r;
+      var tmp;	
+      if(x+dx > w - r - w/40) {
+        x = w-r-w/40;
         dx = - dx;
       }
-      if(x+dx < r) {
-        x = r;
+      if(x+dx < r + w/40) {
+        x = r + w/40;
         dx = - dx;
       }
       if(y+dy > h - r) {
         y = h-r;
         dy = - dy;
+
+        if(dx > 0) {
+          rightY = (tmp = h + (w - x) * dy / dx) > 0 && tmp < h ? tmp : rightY;
+        }
+        else {
+          leftY = (tmp = h - x * dy / dx) > 0 && tmp < h? tmp : leftY;
+        }
       }
       if(y+dy < r) {
         y = r;
         dy = - dy;
+
+        if(dx > 0) {
+          rightY = (tmp = (w - x) * dy / dx) > 0 && tmp < h? tmp : rightY;
+        }
+        else {
+          leftY = (tmp = - x * dy / dx) > 0 && tmp < h? tmp : leftY;
+        }
       }
+
+      leftPad.attr({
+        x: 1,
+        width: w/40,
+        height: h/5
+        });
+
+      rightPad.attr({
+        x: w - w/40 - 1,
+        width: w/40,
+        height: h/5
+        });
+
+      leftPad.transition()
+          .ease("linear")
+          .duration(x)
+          .attr({
+            y: leftY - h/10,
+            });
+
+      rightPad.transition()
+          .ease("linear")
+          .duration(w - x)
+          .attr({
+            y: rightY - h/10
+          });
+
     }
 
     function update() {
       w = root.innerWidth/2;
       h = w / 1.618;
-      
+      r = w / 40;
+
       x += dx;
       y += dy;
       bounce();
@@ -71,12 +137,11 @@
       ball.transition()
           .ease("linear")
           .duration(dt)
-          .attr({cx: x, cy:y})
+          .attr({cx: x, cy:y, r: r})
           .each("end", update);      
-   
+
     }
     update();
-
   }
 
   function load() {
@@ -95,7 +160,7 @@
       }
     }
 
-    route("#bounce", "Bounce");
+    route("#pong", "Pong");
     document.addEventListener("touchmove", function(e) {
       e.preventDefault();
     });
@@ -105,5 +170,3 @@
   root.addEventListener("load", load);
 
 }).call(this);
-
-
