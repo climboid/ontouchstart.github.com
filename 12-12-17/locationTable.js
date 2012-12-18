a8fc2db200b20fbfef84575800422aa1da656a7e
Sam Liu <ontouchstart@gmail.com>
2012-12-17 23:01:57 -0500

    http://ontouchstart.github.com/12-12-17/#locationTable

--- 12-12-17/locationTable.js
+++ 12-12-17/locationTable.js
@@ -61,39 +61,44 @@
         var x = [];
         var y = [];
 
-        function plot(container, x, y, t) {
+        function plot(container, x, y) {
           container.innerHTML = '';
+          var ns = 'http://www.w3.org/2000/svg';
+          var svg = document.createElementNS(ns,'svg');
+      
+          svg.style.width = '500px';
+          svg.style.height = '500px';
+          container.appendChild(svg);
+
+          var path = document.createElementNS(ns, 'path');
+          path.setAttribute('fill', 'none');
+          path.setAttribute('stroke', 'red');
+
+          svg.appendChild(path);
+
           var x_min = x[0];
           var x_max = x[0];
 
           var y_min = y[0];
           var y_max = y[0];
-         
-          var t_min = t[0];
-          var t_max = t[0];
 
-          for(var i = 1; i < t.length; i++) {
+          for(var i = 1; i < x.length; i++) {
             if(x[i] < x_min) { x_min = x[i]; }
             if(x[i] > x_max) { x_max = x[i]; }
             if(y[i] < y_min) { y_min = y[i]; }
             if(y[i] > y_max) { y_max = y[i]; }
-            if(t[i] < t_min) { t_min = t[i]; }
-            if(t[i] < t_max) { t_max = t[i]; }
           }
 
           var epsilon = 0.001;
           range_x = (x_max - x_min) > epsilon? (x_max - x_min) : epsilon;
           range_y = (y_max - y_min) > epsilon? (y_max - y_min) : epsilon;
-
-          for(var i = 0; i < t.length; i++) {
-            var dot = document.createElement('div');
-            dot.innerHTML = '+';
-            dot.style.position = 'absolute';
-            dot.style.left = (x[i] - x_min) / range_x * 400 + 50 + 'px';
-            dot.style.top = 450 - (y[i] - y_min) / range_y * 400 + 'px';
-            container.appendChild(dot);            
+          var d = 'M ';
+          for(var i = 0; i < x.length; i++) {
+            if(i > 0) { d += 'L'; }
+            d += (x[i] - x_min) / range_x * 400 + 50 + ' ';
+            d += 450 - (y[i] - y_min) / range_y * 400;
           }
-
+          path.setAttribute('d', d);
         }
 
         tx.executeSql(sql, [], function(tx, r) {
@@ -129,13 +134,11 @@
              };             
 
              table.appendChild(tr);
-
-             t.push(item.time);
              x.push(item.longitude);
              y.push(item.latitude);
            }
 
-           plot(pathContainer, x, y, t);
+           plot(pathContainer, x, y);
         });
       });
     }
